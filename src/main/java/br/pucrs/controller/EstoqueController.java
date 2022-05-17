dd8c84e5b5c3c49e64257937428b9b1e14bb395b
Aléxia Dorneles <alexiadorneles02@gmail.com>
2022-05-17 20:59:54 -0300

    refactor(estoque): move logic to service

--- src/main/java/br/pucrs/controller/EstoqueController.java
+++ src/main/java/br/pucrs/controller/EstoqueController.java
@@ -1,30 +1,23 @@
 package br.pucrs.controller;
 
-import br.pucrs.domain.entity.ItemEstoque;
 import br.pucrs.domain.entity.Produto;
-import br.pucrs.domain.repository.ItemEstoqueRepository;
+import br.pucrs.service.ItemEstoqueService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/estoque")
 public class EstoqueController {
 
     @Autowired
-    private ItemEstoqueRepository repository;
+    private ItemEstoqueService service;
 
     @GetMapping("/produtos-disponiveis")
     public List<Produto> available() {
-        List<ItemEstoque> itens = repository.findAll();
-        return itens.stream()
-                .filter(item -> item.getQuantidade() > 0)
-                .map(ItemEstoque::getProduto)
-                .collect(Collectors.toList());
+        return this.service.findAvailableProducts();
     }
 }
---
+++ src/main/java/br/pucrs/service/ItemEstoqueService.java
@@ -0,0 +1,27 @@
+package br.pucrs.service;
+
+import br.pucrs.domain.entity.ItemEstoque;
+import br.pucrs.domain.entity.Produto;
+import br.pucrs.domain.repository.ItemEstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ItemEstoqueService {
+
+    @Autowired
+    private ItemEstoqueRepository repository;
+
+    public List<Produto> findAvailableProducts() {
+        return repository.findAll()
+                .stream()
+                .filter(item -> item.getQuantidade() > 0)
+                .map(ItemEstoque::getProduto)
+                .collect(Collectors.toList());
+
+    }
+
+}
