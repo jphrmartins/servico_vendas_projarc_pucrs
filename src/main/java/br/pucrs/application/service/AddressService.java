7ca4c1cc35d94233a1c26431419d864224cbaf8e
Mathias_Kauffmann <mathias_kauffmann@dell.com>
2022-05-20 19:57:16 -0300

    Adding frete calculation

--- src/main/java/br/pucrs/application/service/AddressService.java
+++ src/main/java/br/pucrs/application/service/AddressService.java
@@ -1,12 +1,20 @@
 package br.pucrs.application.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.pucrs.application.constants.StoreAddress;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Component
 public class AddressService {
     final static int R = 6371;
+
+    @Autowired
+    private GeoCodingService geoCodingService;
 
     public static double distanceFromStore(double lat2, double lon2) {
         double latDistance = Math.toRadians(lat2 - StoreAddress.latitude);
@@ -18,14 +26,10 @@ public class AddressService {
         return R * c;
     }
 
-    private static double distanceInKM(double lat1, double lat2, double lon1, double lon2) {
-        double latDistance = Math.toRadians(lat2 - lat1);
-        double lonDistance = Math.toRadians(lon2 - lon1);
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return R * c;
+
+    public double distanceFromStore(String address) {
+        Map<String, Double> coords = geoCodingService.getLatitudeLongitude(address);
+        return distanceFromStore(coords.get("lon"),coords.get("lat"));
     }
 
 }
