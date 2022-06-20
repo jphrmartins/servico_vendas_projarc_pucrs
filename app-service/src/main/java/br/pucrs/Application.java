713a06ac6eb4dc44b0f6700aab4ed7fac9541166
Mathias_Kauffmann <mathias_kauffmann@dell.com>
2022-06-20 20:07:37 -0300

    Adding rabbitMQ queues

--- app-service/src/main/java/br/pucrs/Application.java
+++ app-service/src/main/java/br/pucrs/Application.java
@@ -3,6 +3,7 @@ package br.pucrs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class Application {
 
